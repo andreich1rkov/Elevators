e8448deecc14091516732d718c3904735a8fac92
andreich1rkov <andrmen04@gmail.com>
2023-12-12 23:00:30 +0500

    Create elevators.py

---
+++ elevators.py
@@ -0,0 +1,58 @@
+class House:
+    def __init__(self, address, floors, elevators):
+        self.address = address
+        self.floors = floors
+        self.elevators = elevators
+  
+class Elevator:
+    def __init__(self, capacity, current_floor, direction, destination, load):
+        self.capacity = capacity
+        self.current_floor = current_floor
+        self.direction = direction
+        self.destination = destination
+        self.load = load
+    def display_info(self, capacity, current_floor, direction, destination, load):
+        print(f"Информация о лифте {self}: вместимость - {capacity}, текущий этаж - {current_floor}, направление движения - {direction}, назначенный этаж - {destination}, загрузка - {load}")
+
+class Operator:
+    def track_elevator_position(self, elevator):
+        print(f"Лифт находится на этаже № {elevator.current_floor}")
+
+    def send_elevator_to_floor(self, elevator, floor):
+        if floor > elevator.current_floor:
+            elevator.direction = "вверх"
+        elif floor < elevator.current_floor:
+            elevator.direction = "вниз"
+        else:
+            elevator.direction = "stationary"
+        elevator.current_floor = floor
+        print(f"Отправляем лифт на этаж № {floor}")
+
+
+
+
+house1 = House("Шейкмана, 69", 25, 2)
+house2 = House("Ленина, 40", 9, 1)
+house3 = House("Пушкина, 1", 12, 3)
+
+
+elevator1 = Elevator(1000, 1, "stationary", 0, 0)
+elevator2 = Elevator(800, 1, "stationary", 0, 0)
+elevator3 = Elevator(1200, 1, "stationary", 0, 0)
+
+
+operator = Operator()
+
+
+operator.track_elevator_position(elevator1)
+operator.send_elevator_to_floor(elevator1, 5)
+operator.track_elevator_position(elevator1)
+
+operator.track_elevator_position(elevator2)
+operator.send_elevator_to_floor(elevator2, 3)
+operator.track_elevator_position(elevator2)
+
+operator.track_elevator_position(elevator3)
+operator.send_elevator_to_floor(elevator3, 8)
+operator.track_elevator_position(elevator3)
+
