78b7d8cc5ba7dcf850e14db3aab44b8c2a3b0423
andreich1rkov <andrmen04@gmail.com>
2023-12-13 16:44:19 +0500

    Update elevators.py

--- elevators.py
+++ elevators.py
@@ -1,11 +1,13 @@
 import random
 
+# Класс House включает: адресс, кол-во этажей, кол-во лифтов
 class House:
     def __init__(self, address, floors, elevators):
         self.address = address
         self.floors = floors
         self.elevators = elevators
   
+# Класс Elevator включает: вместимость(грузоподъемность), текущий этаж, направление движения, этаж назначения, состояние дверей (заблок./разблок.) 
 class Elevator:
     def __init__(self, capacity, current_floor, direction, destination):
         self.capacity = capacity
@@ -14,19 +16,23 @@ class Elevator:
         self.destination = destination
         self.doors = False
 
+# Класс Operator включает: функцию отображения состояния лифта, функции управления лифтом (отправление на этаж, управление дверьми, экстренная остановка лифта)
 class Operator:
     def elevator_info(self, elevator):
         if elevator.direction == "не двигается":
             print(f"Информация о лифте: вместимость - {elevator.capacity}, текущий этаж - {elevator.current_floor}, направление движения - {elevator.direction}")
             print(f"Двери заблокированы: {elevator.doors}")
+            print()
         else:
             print(f"Информация о лифте: вместимость - {elevator.capacity}, направление движения - {elevator.direction}, назначенный этаж - {elevator.destination}")
             print(f"Лифт добрался до назначенного этажа {elevator.current_floor}")
+            print()
             elevator.direction = "не двигается"   
 
     def send_elevator_to_floor(self, elevator, floor):
         if elevator.doors == True:
             print("Чтобы отправить лифт на другой этаж, разблокируйте двери")
+            print()
             return
 
         if floor > elevator.current_floor:
@@ -39,56 +45,65 @@ class Operator:
         elevator.current_floor = floor
         elevator.destination = floor
         print(f"Отправляем лифт на этаж № {floor}")
+        print()
     
     def elevator_stop(self, elevator):
         if elevator.direction == "не двигается":
             print("Лифт и так стоит на месте.")
+            print()
         else:
             elevator.current_floor = random.randint(elevator.start_floor, elevator.destination)
             elevator.direction = "не двигается"
             print(f"Лифт экстренно остановлен на этаже {elevator.current_floor}")
+            print()
 
 
     def unblock_doors(self, elevator):
         elevator.doors = False
         print("Двери разблокированы")
+        print()
       
 
     def block_doors(self, elevator):
         elevator.doors = True
-        elevator.direction = "не двигается"
         print("Двери заблокированы")
+        print()
 
 
 
 
 
-house1 = House("Шейкмана, 69", 25, 2)
-house2 = House("Ленина, 40", 9, 1)
-house3 = House("Пушкина, 1", 12, 3)
 
+import tkinter as tk
 
-elevator1 = Elevator(1000, 1, "не двигается", 1)
-elevator2 = Elevator(800, 1, "не двигается", 1)
-elevator3 = Elevator(1200, 1, "не двигается", 1)
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.create_widgets()
 
+    def create_widgets(self):
+        self.quit_button = tk.Button(self, text="Quit", command=self.master.destroy)
+        self.quit_button.pack(side="bottom")
 
-operator = Operator()
+        self.context_menu = tk.Menu(self, tearoff=0)
+        self.context_menu.add_command(label="Отобразить информацию о лифте", command=self.show_elevator_info)
+        self.context_menu.add_command(label="Отправить лифт на этаж", command=self.send_elevator)
 
-operator.elevator_info(elevator1)
-operator.send_elevator_to_floor(elevator1, 5)
-operator.elevator_stop(elevator1)
-operator.elevator_info(elevator1)
-operator.elevator_info(elevator1)
-print()
+        self.bind_context_menu(self.quit_button)
 
-operator.block_doors(elevator1)
-operator.send_elevator_to_floor(elevator1, 6)
-operator.elevator_info(elevator1)
-print()
+    def show_context_menu(self, event):
+        self.context_menu.post(event.x_root, event.y_root)
+    
+    def bind_context_menu(self, widget):
+        widget.bind("<Button-3>", self.show_context_menu)
 
-operator.unblock_doors(elevator1)
-operator.elevator_info(elevator1)
-operator.send_elevator_to_floor(elevator1, 1)
-operator.elevator_info(elevator1)
+    def show_elevator_info(self):
+        pass# Ваш код для отображения информации о лифте
 
+    def send_elevator(self):
+        pass# Ваш код для отправки лифта на этаж
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
